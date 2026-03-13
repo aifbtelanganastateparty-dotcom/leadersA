e364b8e10f62047e5ca1d8d7875cece896a5b728
Developer <dev@leadersforindia.org>
2026-03-13 15:40:51 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
+  base: '/leadersA/',
   plugins: [imagetools()],
   build: {
     // Optimization for smaller builds
